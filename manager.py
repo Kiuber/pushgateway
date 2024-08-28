6ee0a507f974c97a2a36e4552ebb62a6f504bc42
kiuber <kiuber.zhang@gmail.com>
2024-08-29 00:07:59 +0800

    feat: add manager.py

---
+++ manager.py
@@ -0,0 +1,26 @@
+from echobox.tool import functocli
+from echobox.app.devops import DevOpsApp
+
+APP_NAME = 'prom-pushgateway-ttl'
+
+
+class App(DevOpsApp):
+
+    def __init__(self):
+        DevOpsApp.__init__(self, APP_NAME)
+
+    def build_image(self, version, platform='linux/amd64', push=False):
+        image_tag = f'{version}-ttl'
+        image_name = f'kiuber/{self.app_name}'
+        image = f'{image_name}:{image_tag}'
+        self.shell_run(f'PREFIX=./.build/{platform.replace("/", "-")} make build')
+
+        env = f'DOCKER_ARCHS=amd64 DOCKER_IMAGE_NAME={image_name} SANITIZED_DOCKER_IMAGE_TAG="" DOCKER_IMAGE_TAG={image_tag}'
+        self.shell_run(f'{env} make common-docker')
+
+        if push:
+            self.shell_run(f'{env} make common-docker-publish')
+
+
+if __name__ == '__main__':
+    functocli.run_app(App)
